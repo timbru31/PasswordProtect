b72841aee0690c6e2f5b280cbff6aa22ef17b9ce
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2015-06-04 10:40:06 +0200

    Removed redundant modifier

--- src/main/java/de/dustplanet/passwordprotect/IJailLocation.java
+++ src/main/java/de/dustplanet/passwordprotect/IJailLocation.java
@@ -6,11 +6,11 @@ public interface IJailLocation {
      * Returns current radius for the JailLocation.
      * @return the radius
      */
-    abstract int getRadius();
+    int getRadius();
 
     /**
      * Sets an updated radius of the JailLocation.
      * @param radius new radius
      */
-    abstract void setRadius(int radius);
+    void setRadius(int radius);
 }
